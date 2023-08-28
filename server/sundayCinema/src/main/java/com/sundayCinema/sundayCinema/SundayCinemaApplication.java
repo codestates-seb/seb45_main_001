731bd5aa897738d1f26acd5c18fe572711b24d98
ParkChanWoo <129935701+chanwoopark9301@users.noreply.github.com>
2023-08-28 17:56:40 +0900

    [BE] 백엔드 템플릿 추가

---
+++ server/sundayCinema/src/main/java/com/sundayCinema/sundayCinema/SundayCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.sundayCinema.sundayCinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SundayCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SundayCinemaApplication.class, args);
+	}
+
+}
