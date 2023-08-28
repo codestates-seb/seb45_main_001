86c5aa7f93792f2c544a3bbca91e6e1b824a157b
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 <gmlahr970516@naver.com>
2023-08-28 15:43:03 +0900

    레이아웃 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,15 @@
-
-import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Content from './components/Content';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="layout">
+      <Header />
+      <Content/>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,1 +1,10 @@
-export {};
+import { HeaderStyle } from './styled';
+
+const Header = () => {
+  return (
+    <>
+      <HeaderStyle>헤더 입니다.</HeaderStyle>
+    </>
+  );
+};
+export default Header;
