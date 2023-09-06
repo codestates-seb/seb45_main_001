7f17bdb580b7d91b7b11ae2952c15125ac0df9e4
jinyong1015 <jjyim1015@gmail.com>
2023-09-06 17:32:05 +0900

    feat: Row 페이지 구현

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,17 +3,18 @@ import Footer from './components/Footer';
 import Submain from './components/Submain';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import "./css/App.css";
 
 function App() {
     return (
-        <>
+        <div className="app">
             {/* <Header /> */}
             <Routes>
                 <Route path="/*" element={<HomePage />} />
                 <Route path="/submain" element={<Submain />} />
             </Routes>
             <Footer />
-        </>
+        </div>
     );
 }
 
