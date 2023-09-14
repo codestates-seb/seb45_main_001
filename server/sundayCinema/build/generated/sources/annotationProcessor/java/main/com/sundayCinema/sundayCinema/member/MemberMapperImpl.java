4ddc87541b989578aab9c89e59c1b22a24580f19
JangJeonggyu <jeonggyu116@gmail.com>
2023-09-14 23:00:13 +0900

    test2

---
+++ server/sundayCinema/build/generated/sources/annotationProcessor/java/main/com/sundayCinema/sundayCinema/member/MemberMapperImpl.java
@@ -0,0 +1,82 @@
+package com.sundayCinema.sundayCinema.member;
+
+import com.sundayCinema.sundayCinema.member.MemberDto.MemberPatchDto;
+import com.sundayCinema.sundayCinema.member.MemberDto.MemberPostDto;
+import com.sundayCinema.sundayCinema.member.MemberDto.MemberResponseDto;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-09-14T21:25:41+0900",
+    comments = "version: 1.4.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.2.1.jar, environment: Java 11.0.18 (Azul Systems, Inc.)"
+)
+@Component
+public class MemberMapperImpl implements MemberMapper {
+
+    @Override
+    public Member memberPostDtoToMember(MemberPostDto memberPostDto) {
+        if ( memberPostDto == null ) {
+            return null;
+        }
+
+        Member member = new Member();
+
+        member.setUserName( memberPostDto.getUserName() );
+        member.setEmail( memberPostDto.getEmail() );
+        member.setPassword( memberPostDto.getPassword() );
+
+        return member;
+    }
+
+    @Override
+    public Member memberPatchDtoToMember(MemberPatchDto memberPatchDto) {
+        if ( memberPatchDto == null ) {
+            return null;
+        }
+
+        Member member = new Member();
+
+        member.setUserName( memberPatchDto.getUserName() );
+        member.setPassword( memberPatchDto.getPassword() );
+
+        return member;
+    }
+
+    @Override
+    public MemberResponseDto memberToMemberResponseDto(Member member) {
+        if ( member == null ) {
+            return null;
+        }
+
+        long memberId = 0L;
+        String email = null;
+        String userName = null;
+        String password = null;
+
+        memberId = member.getMemberId();
+        email = member.getEmail();
+        userName = member.getUserName();
+        password = member.getPassword();
+
+        MemberResponseDto memberResponseDto = new MemberResponseDto( memberId, email, userName, password );
+
+        return memberResponseDto;
+    }
+
+    @Override
+    public List<MemberResponseDto> membersToMemberResponses(List<Member> members) {
+        if ( members == null ) {
+            return null;
+        }
+
+        List<MemberResponseDto> list = new ArrayList<MemberResponseDto>( members.size() );
+        for ( Member member : members ) {
+            list.add( memberToMemberResponseDto( member ) );
+        }
+
+        return list;
+    }
+}
