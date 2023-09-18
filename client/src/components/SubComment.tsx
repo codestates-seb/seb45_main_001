42765eaf1ee16d4a082cbd3e843f46dfb2da5d47
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 <gmlahr970516@naver.com>
2023-09-18 18:11:02 +0900

    feat:댓글/별점

--- client/src/components/SubComment.tsx
+++ client/src/components/SubComment.tsx
@@ -1,6 +1,7 @@
-import React, { useState, ChangeEvent } from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
 import { styled } from 'styled-components';
-
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 const Textform = styled.div`
   display: flex;
@@ -100,9 +101,11 @@ const TextListli = styled.li`
 `;
 
 const SubComment = () => {
-  const starRatings: number[] = [1, 2, 3, 4, 5]; // 별점 옵션 배열
+  const starRatings: number[] = [1, 2, 3, 4, 5];
   const [selectedRating, setSelectedRating] = useState<number | null>(null);
   const [comment, setComment] = useState<string>('');
+  const { movieId } = useParams(); // URL 파라미터인 'movieId'를 읽어옵니다.
+  const [commentsList, setCommentsList] = useState<{ starRating: number; comment: string }[]>([]);
 
   const handleStarRating = (rating: number) => {
     setSelectedRating(rating);
@@ -112,21 +115,68 @@ const SubComment = () => {
     setComment(e.target.value);
   };
 
-  const handleCommentSubmit = () => {
+  
+
+  const handleCommentSubmit = async () => {
     if (selectedRating !== null) {
       const now = new Date();
       const timestamp = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
       const newComment = {
         starRating: selectedRating,
-        comment: `${comment} - ${timestamp}`, // 댓글과 시간을 결합하여 저장
+        comment: `${comment} - ${timestamp}`,
       };
-      setCommentsList(prevComments => [...prevComments, newComment]);
-      setComment('');
-      setSelectedRating(null);
+  
+
+      // 인증 토큰을 가져오기
+      const authToken = localStorage.getItem("jwt");
+  
+      // 인증 토큰을 헤더에 추가
+      const config = {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      };
+  
+      // API 엔드포인트 및 데이터 전송
+      try {
+        await axios.post(
+          `http://localhost:8080/api/comments/${movieId}=1&userId=1`,
+          newComment,
+          config
+        );
+  
+        // 성공적으로 댓글이 추가되었을 때 처리
+        console.log('댓글이 성공적으로 추가되었습니다.');
+  
+        // 댓글 목록 업데이트 등의 작업 수행
+        setCommentsList(prevComments => [...prevComments, newComment]);
+        setComment('');
+        setSelectedRating(null);
+      } catch (error) {
+        console.error('댓글 추가 중 오류 발생:', error);
+        // 오류 처리
+      }
     }
   };
 
-  const [commentsList, setCommentsList] = useState<{ starRating: number; comment: string }[]>([]);
+  // 컴포넌트가 처음 렌더링될 때 댓글을 가져오는 useEffect
+  useEffect(() => {
+    // 댓글을 가져오는 API 엔드포인트 주소
+    const apiUrl = `http://13.209.157.148:8080/api/comments/movie/${movieId}`;
+
+    // API 요청 보내기
+    axios.get(apiUrl)
+      .then(response => {
+        // 성공적으로 댓글을 가져왔을 때 댓글 목록 업데이트
+        setCommentsList(response.data);
+      })
+      .catch(error => {
+        console.error('댓글 가져오기 오류:', error);
+        // 오류 처리
+      });
+  }, [movieId]); // movieId가 변경될 때마다 useEffect가 다시 실행됨
+
+  // 나머지 함수들은 이전과 동일하게 유지
 
   return (
     <>
@@ -134,15 +184,15 @@ const SubComment = () => {
         <TextBox>
           <TextBox2>
             <Starbox>
-            {starRatings.map((rating, index) => (
-              <Text1
-                key={index}
-                onClick={() => handleStarRating(rating)}
-                active={rating <= (selectedRating ?? 0)} // null 별점 처리
-              >
-                ☆
-              </Text1>
-            ))}
+              {starRatings.map((rating, index) => (
+                <Text1
+                  key={index}
+                  onClick={() => handleStarRating(rating)}
+                  active={rating <= (selectedRating ?? 0)}
+                >
+                  ☆
+                </Text1>
+              ))}
             </Starbox>
             <Text2 onClick={handleCommentSubmit}>댓글등록</Text2>
           </TextBox2>
